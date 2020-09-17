e0046e14382a311c188a2a2e4bc7102b80fd93ff
RafaelFerrao <r.mouracruz@gmail.com>
2020-09-17 17:00:56 -0300

    only run as main module

--- src/d01_cleanData/data_cleansy.py
+++ src/d01_cleanData/data_cleansy.py
@@ -6,41 +6,42 @@ from pathlib import Path
 import sys
 sys.path.append('../..')
 
-DATA_FOLDER = Path.cwd().joinpath('data')
-read_data_path = Path.joinpath(DATA_FOLDER, 'despesas56.pkl')
-df = pd.read_pickle(read_data_path)
+if __name__ == '__main__':
+    DATA_FOLDER = Path.cwd().joinpath('data')
+    read_data_path = Path.joinpath(DATA_FOLDER, 'despesas56.pkl')
+    df = pd.read_pickle(read_data_path)
 
-#%%
-mapped_remaining_info = df["ultimoStatus"].apply(lambda x: (x['siglaPartido'], 
-                                                            x['siglaUf'],
-                                                            x["condicaoEleitoral"], 
-                                                            x['situacao'], 
-                                                            x['descricaoStatus'], 
-                                                            x['nomeEleitoral'])).values
+    #%%
+    mapped_remaining_info = df["ultimoStatus"].apply(lambda x: (x['siglaPartido'], 
+                                                                x['siglaUf'],
+                                                                x["condicaoEleitoral"], 
+                                                                x['situacao'], 
+                                                                x['descricaoStatus'], 
+                                                                x['nomeEleitoral'])).values
 
 
-mapped_remaining_info_stacked = np.vstack(mapped_remaining_info)
+    mapped_remaining_info_stacked = np.vstack(mapped_remaining_info)
 
-df["partido"], df['ufPartido'], df["condicao"], df["situacao"], df["status"], df["nomeEleitoral"] = np.hsplit(mapped_remaining_info_stacked, 6)
+    df["partido"], df['ufPartido'], df["condicao"], df["situacao"], df["status"], df["nomeEleitoral"] = np.hsplit(mapped_remaining_info_stacked, 6)
 
-features = ['id', 
-            'nomeEleitoral', 
-            'partido', 
-            'ufPartido',
-            'sexo', 
-            'dataNascimento', 
-            'ufNascimento', 
-            'municipioNascimento', 
-            'escolaridade', 
-            'codDocumento',
-            'numDocumento',
-            'valorDocumento',
-            'valorLiquido',
-            'valorGlosa',
-            'tipoDespesa', 
-            'nomeFornecedor', 
-            'dataDocumento',
-            'mes',
+    features = ['id', 
+                'nomeEleitoral', 
+                'partido', 
+                'ufPartido',
+                'sexo', 
+                'dataNascimento', 
+                'ufNascimento', 
+                'municipioNascimento', 
+                'escolaridade', 
+                'tipoDocumento',
+                'numDocumento',
+                'valorDocumento',
+                'valorLiquido',
+                'valorGlosa',
+                'tipoDespesa', 
+                'nomeFornecedor', 
+                'dataDocumento',
+                'mes',
             'parcela', 
             'condicao', 
             'situacao', 
@@ -64,5 +65,3 @@ df_clean['idade'] = 2020 - df_clean['nascimento'].dt.year
 
 save_data_path = Path.joinpath(DATA_FOLDER, 'despesas56_clean.pkl')
 df_clean.to_pickle(save_data_path)
-
-# %%
